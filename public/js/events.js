0bc943c6f82e802470aa04704de16fa166ef3694
Rahul Kumar Patro <rahulkpatro@gmail.com>
2019-10-17 21:07:53 +0530

    updated gallery

--- public/js/events.js
+++ public/js/events.js
@@ -1,7 +1,15 @@
-$(window).load(function() {
-            $(".se-pre-con").fadeOut();;
-        });
+//Preloader
 
+$(window).on('load', function() { // makes sure the whole site is loaded
+    setTimeout(function(){ 
+        $('#status').fadeOut(); // will first fade out the loading animation 
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+        $('body').delay(350).css({'overflow':'visible'});
+     }, 3000);
+
+})
+
+//
 
 function openNav() {
    
--- public/js/gallery.js
+++ public/js/gallery.js
@@ -29,7 +29,7 @@ $(window).on('load', function() { // makes sure the whole site is loaded
         $('#status').fadeOut(); // will first fade out the loading animation 
         $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
         $('body').delay(350).css({'overflow':'visible'});
-     }, 3000);
+     }, 0);
 
 })
 
