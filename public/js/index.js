65a29214440c1664e73760e9af105409db852b4f
skittu <kartiksinghal15@gmail.com>
2019-10-17 17:07:50 +0530

    photos removed preloader improved countdown added

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,21 @@
+
+
+$(window).on('load', function() { // makes sure the whole site is loaded
+        setTimeout(function(){ 
+            $('#status').fadeOut(); // will first fade out the loading animation 
+            $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+            $('body').delay(350).css({'overflow':'visible'});
+         }, 3000);
+   
+  })
+
+
+
+
+
+  
+
+
 var bas=1;
 
 $(function () {
@@ -183,21 +201,67 @@ $(window).load(function() {
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
-    x = setInterval(function() {
+// let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
+//     x = setInterval(function() {
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+//       let now = new Date().getTime(),
+//           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+//       document.getElementById('days').innerText = Math.floor(distance / (day)),
+//         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+//         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+//         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
       
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+//       //do something later when date is reached
+//       //if (distance < 0) {
+//       //  clearInterval(x);
+//       //  'IT'S MY BIRTHDAY!;
+//       //}
 
-    }, second)
+//     }, second)
+
+
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  var deadline = new Date(2019,10,8,0,0,0,0);
+  initializeClock('clockdiv', deadline);
+
+
